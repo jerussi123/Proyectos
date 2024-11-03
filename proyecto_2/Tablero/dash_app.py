34f48a8ce2f9f2bc3faf07c29524fed9762dc9b1
Jorge Russi Russi <je.russi@uniandes.edu.co>
2024-11-03 17:16:34 -0500

    base dash and libraries

---
+++ proyecto_2/Tablero/dash_app.py
@@ -0,0 +1,53 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import plotly.graph_objects as go
+import math  
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
+
+home_layout = html.Div([html.H1("Home"), html.P("Welcome to the Home page!")])
+data_exploration_layout = html.Div([html.H1("Data Exploration"), html.P("Explore your data here!")])
+predictions_layout = html.Div([html.H1("Predictions"), html.P("Make predictions with your data!")])
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    dbc.NavbarSimple(
+        children=[
+            dbc.NavItem(dbc.NavLink("Home", href="/home", active=True)),
+            dbc.NavItem(dbc.NavLink("Data Exploration", href="/data-exploration")),
+            dbc.NavItem(dbc.NavLink("Predictions", href="/predictions")),
+            dbc.Form(className="d-flex me-2", children=[
+                dcc.Input(type="search", placeholder="Search", className="form-control me-2"),
+                dbc.Button("Search", color="secondary", className="my-2 my-sm-0"),
+            ])
+        ],
+        brand="Bank Data Analytics",
+        brand_href="#",
+        color="primary",
+        dark=True,
+        expand="lg",
+        className="mb-4",
+    ),
+    html.Div(id='page-content') #
+])
+
+# Callback de la barra de navegacion
+@app.callback(
+    Output('page-content', 'children'),
+    [Input('url', 'pathname')]
+)
+def display_page(pathname):
+    if pathname == '/home':
+        return home_layout
+    elif pathname == '/data-exploration':
+        return data_exploration_layout
+    elif pathname == '/predictions':
+        return predictions_layout
+    else:
+        return home_layout  # Home si no funciona
+    
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8050)
